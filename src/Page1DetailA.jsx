a123e9e4a4fb15f1f0539bb8892ba401b5426d0c
matsumoto-mi <106508297+matsumoto-mi@users.noreply.github.com>
2022-07-14 15:46:45 +0900

    セクション5まで終了

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { useLocation, useHistory } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const { state } = useLocation();
+  console.log(state);
+  const history = useHistory();
+  const onClickBuck = () => history.goBack();
   return (
     <div>
       <h1>Page1DetailAページです</h1>
+      <button onClick={onClickBuck}>戻る</button>
     </div>
   );
 };
